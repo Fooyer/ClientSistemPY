d32f59be6496928efc920fb18eb2efa19b057edd
Freddy Baier <xneerd157@gmail.com>
2022-12-05 09:28:23 -0300

    Criação da estrutura de pessoa

--- Dados/pessoa.py
+++ Dados/pessoa.py
@@ -1,1 +1,41 @@
 # Classe main de pessoa, todas as outras classes que tem relação com pessoa deverão herdar dessa
+
+
+class Pessoa:
+
+    __nome = None
+    __endereco = None
+    __email = None
+    __telefone = None
+
+    def setNome(self,nome):
+
+        __nome=nome
+
+    def getNome(self):
+
+        return __nome
+          
+    def setEndereco(self,endereco):
+
+        __endereco=endereco
+
+    def getEndereco(self):
+
+        return __endereco
+          
+    def setEmail(self,email):
+
+        __email=email
+
+    def getEmail(self):
+
+        return __email
+          
+    def setTelefone(self,telefone):
+
+        __telefone=telefone
+
+    def getTelefone(self):
+
+        return __telefone
