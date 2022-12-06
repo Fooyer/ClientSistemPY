db1088e4305284dced9f13d6e355ee4e4a5fe170
Freddy Baier <xneerd157@gmail.com>
2022-12-06 08:31:08 -0300

    Implementação do loop de menu

--- menu.py
+++ menu.py
@@ -2,7 +2,7 @@
 
 # Importar as classes usadas no código
 
-import time
+import time,os
 from Dados.clienteFisico import clienteFisico
 from Dados.clienteJuridico import clienteJuridico
 
@@ -11,85 +11,114 @@ from Dados.clienteJuridico import clienteJuridico
 clienteJuridico=clienteJuridico()
 clienteFisico=clienteFisico()
 
-# Pergunta de qual ação o usuário deseja fazer
+# Função que faz o menu do programa
 
-acao = input("Qual ação deseja efetuar? \n\n 1 - Cadastrar novo cliente \n 2 - Excluir cliente \n 3 - Editar cliente \n\n Resposta: ")
+def programa():
 
-# Match para definir a descrição da ação a apresentar na definição da classificação do cliente
+    while True:
 
-match(acao):
-    
-    case "1":
+        # Limpar o console do sistema
         
-        descricaoAcao="Cadastrar"
+        os.system('clear')
         
-    case "2":
+        # Pergunta se deseja sair da execução (perderá todos os dados)
         
-        descricaoAcao="Excluir"
+        sair=input("Deseja Sair?(S/N): ")
+        if sair=="S":
+            return "S"
         
-    case "3":
-        
-        descricaoAcao="Editar"
-        
-    case _:
-        
-        print("\nAção Inválida!\n")
-        descricaoAcao = None
-        time.sleep(3)
-        exit()
+        # Pergunta de qual ação o usuário deseja fazer
+         
+        acao = input("Qual ação deseja efetuar? \n\n 1 - Cadastrar novo cliente \n 2 - Excluir cliente \n 3 - Editar cliente \n\n Resposta: ")
 
-# Seleção de classificação do Cliente
+        # Match para definir a descrição da ação a apresentar na definição da classificação do cliente
 
-classificacao = input("\n\n"+descricaoAcao+" cliente: \n\n 1 - Jurídico \n 2 - Físico \n\n Resposta: ")
+        match(acao):
+            
+            case "1":
+                
+                descricaoAcao="Cadastrar"
+                
+            case "2":
+                
+                descricaoAcao="Excluir"
+                
+            case "3":
+                
+                descricaoAcao="Editar"
+                
+            case _:
+                
+                print("\nAção Inválida!\n")
+                descricaoAcao = None
+                time.sleep(3)
+                return "N"
 
-# Gera os dados da classificação escolhida pelo cliente
+        # Seleção de classificação do Cliente
 
-if (classificacao=="1"):
+        classificacao = input("\n\n"+descricaoAcao+" cliente: \n\n 1 - Jurídico \n 2 - Físico \n\n Resposta: ")
 
-    listaClientes=clienteJuridico.getDados()
-
-# Apresenta a lista de clientes com a classificação no sistema se a opção não for de cadastro
-
-
-if (acao!=1):
-    for identificador,cliente in listaClientes:
-    
-        print(identificador," - ",cliente)
-
-
-match(acao):
-    
-    case "1":
-        
-        nomeCliente = input("Nome do cliente: ")
-        enderecoCliente = input("Endereço do cliente: ")
-        emailCliente = input("Email do cliente: ")
-        telefoneCliente = input("Telefone do cliente: ")
+        # Gera os dados da classificação escolhida pelo cliente
 
         if (classificacao=="1"):
+
+            listaClientes=clienteJuridico.getDados()
+
+        # Apresenta a lista de clientes com a classificação no sistema se a opção não for de cadastro
+
+
+        if (acao!=1):
+            for identificador,cliente in enumerate(listaClientes):
             
-            cpnjCliente = input("CNPJ do cliente: ")
-
-            clienteJuridico.setCnpj(cpnjCliente)
-            clienteJuridico.setEmail(emailCliente)
-            clienteJuridico.setEndereco(enderecoCliente)
-            clienteJuridico.setNome(nomeCliente)
-            clienteJuridico.setTelefone(telefoneCliente)
-
-            clienteJuridico.setDados()
-            
-            dados=clienteJuridico.getDados()
-
-            for identificador,cliente in enumerate(dados):
-    
                 print(identificador," - ",cliente)
 
-            time.sleep(10)
+        match(acao):
+            
+            case "1":
+                
+                nomeCliente = input("Nome do cliente: ")
+                enderecoCliente = input("Endereço do cliente: ")
+                emailCliente = input("Email do cliente: ")
+                telefoneCliente = input("Telefone do cliente: ")
+
+                if (classificacao=="1"):
+                    
+                    cpnjCliente = input("CNPJ do cliente: ")
+
+                    clienteJuridico.setCnpj(cpnjCliente)
+                    clienteJuridico.setEmail(emailCliente)
+                    clienteJuridico.setEndereco(enderecoCliente)
+                    clienteJuridico.setNome(nomeCliente)
+                    clienteJuridico.setTelefone(telefoneCliente)
+
+                    clienteJuridico.setDados()
+                    
+                    dados=clienteJuridico.getDados()
+
+                    for identificador,cliente in enumerate(dados):
+            
+                        print(identificador," - ",cliente)
+                
+                elif (classificacao=="2"):
+                    
+                    clienteFisico.setCnpj(cpnjCliente)
+                    clienteFisico.setEmail(emailCliente)
+                    clienteFisico.setEndereco(enderecoCliente)
+                    clienteFisico.setNome(nomeCliente)
+                    clienteFisico.setTelefone(telefoneCliente)
+                    
+            case "2":
+                
+                print(acao)
+                
+            case "3":
+                
+                print(acao)
         
-    case "2":
+
+# Executa o programa e faz o controle de saída
         
-        print(acao)
-        
-    case "3":
-        
-        print(acao)
+while True:
+    sair=programa()
+    if (sair=="S"):
+        break
